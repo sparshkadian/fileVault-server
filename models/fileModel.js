6e619ae8cd60b08811c6bf33985ee42f18d4866b
Sparsh Kadian <sparshkadian323@gmail.com>
2024-03-21 16:35:43 +0530

    created fileModel

--- models/fileModel.js
+++ models/fileModel.js
@@ -1,6 +1,23 @@
 import mongoose from 'mongoose';
 
-const fileSchema = mongoose.Schema({});
+const fileSchema = mongoose.Schema(
+  {
+    fileName: { type: String, required: true },
+
+    fileType: { type: String, required: true },
+
+    fileSize: { type: Number, required: true },
+
+    starred: { type: Boolean, default: false },
+
+    inTrash: { type: Boolean, default: false },
+
+    downloadLink: String,
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const File = mongoose.model('File', fileSchema);
 
