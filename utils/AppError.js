35bc83c4cfd4fd6eabcc24864586cfd3f7fb8a99
Sparsh Kadian <sparshkadian323@gmail.com>
2024-03-21 19:03:36 +0530

    added AppError class

---
+++ utils/AppError.js
@@ -0,0 +1,12 @@
+class AppError extends Error {
+  constructor(message, statusCode) {
+    super(message);
+    this.statusCode = statusCode || 500;
+    this.status = statusCode >= 400 && statusCode <= 499 ? 'fail' : 'error';
+    this.isOperational = true;
+
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+export default AppError;
