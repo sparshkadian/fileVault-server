d4906ef891b67f3a72a884e053094e087c804a10
Sparsh Kadian <sparshkadian323@gmail.com>
2024-03-21 16:32:22 +0530

    created userModel

--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,41 @@
 import mongoose from 'mongoose';
 
-const userSchema = mongoose.Schema({});
+const userSchema = mongoose.Schema(
+  {
+    userName: {
+      type: String,
+      required: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: {
+        validator: function (email) {
+          return /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g.test(email);
+        },
+      },
+      message: (props) => `${props.value} is not a valid email`,
+    },
+
+    avatar:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-DSW54utMSZ6J1F9luVr6YYDoRZ-FQYCL3w&usqp=CAU',
+
+    password: {
+      type: String,
+      required: true,
+    },
+
+    confirmPassword: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const User = mongoose.model('User', userSchema);
 
