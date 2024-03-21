b6d3620874b82edbd99cd93ea7a9175be3da472b
Sparsh Kadian <sparshkadian323@gmail.com>
2024-03-21 19:02:30 +0530

    added global error handler

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ import express from 'express';
 import morgan from 'morgan';
 
 import authRouter from './routes/authRoutes.js';
+import globalErrorHandler from './controllers/errorController.js';
 
 const app = express();
 app.use(express.json());
 app.use(morgan('dev'));
 
 app.use('/api/auth', authRouter);
+
+app.use(globalErrorHandler);
 
 export default app;
