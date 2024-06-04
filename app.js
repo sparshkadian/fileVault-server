b87b631c03821e8d073ed759136045a4a3f808f5
Sparsh Kadian <sparshkadian323@gmail.com>
2024-06-04 23:12:37 +0530

    added client origin for cors

--- app.js
+++ app.js
@@ -9,7 +9,11 @@ import globalErrorHandler from './controllers/errorController.js';
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: 'https://file-vault-client.vercel.app/',
+  })
+);
 app.use(morgan('dev'));
 
 app.use('/api/auth', authRouter);
