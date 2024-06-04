0da2d18ca018e5a39882a6851c60630a054e801e
Sparsh Kadian <sparshkadian323@gmail.com>
2024-06-04 23:15:43 +0530

    removed origin did not work as expected

--- app.js
+++ app.js
@@ -9,11 +9,7 @@ import globalErrorHandler from './controllers/errorController.js';
 
 const app = express();
 app.use(express.json());
-app.use(
-  cors({
-    origin: 'https://file-vault-client.vercel.app/',
-  })
-);
+app.use(cors());
 app.use(morgan('dev'));
 
 app.use('/api/auth', authRouter);
